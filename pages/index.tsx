a5507d41d9e3bb97a0d42e95800273acf71a4c7b
Clifton King <cliftonk@gmail.com>
2021-03-18 18:54:56 -0500

    add social links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,29 @@
-import { Box, Grid, Text, VStack, Image } from '@chakra-ui/react';
-import { FunctionComponent } from 'react';
+import {
+  Box,
+  Flex,
+  Grid,
+  Icon,
+  Image,
+  Link,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import { ElementType, FunctionComponent } from 'react';
+import { SiInstagram, SiMailDotRu } from 'react-icons/si';
 import { Layout } from '../components/Layout';
+
+const SocialLink: FunctionComponent<{
+  href: string;
+  label: string;
+  text: string;
+  icon: ElementType;
+}> = ({ href, label, text, icon }) => (
+  <Link href={href}>
+    <Icon as={icon} aria-label={label} mr={1} />
+    {text}
+  </Link>
+);
 
 const IndexPage: FunctionComponent = () => (
   <Layout title="Breakthrough Healing">
@@ -24,6 +47,23 @@ const IndexPage: FunctionComponent = () => (
           >
             By Dr. Juliana King, DACM
           </Text>
+          <Flex pt={2}>
+            <Text pr={3}>Contact me:</Text>
+            <Stack textAlign="left">
+              <SocialLink
+                href="https://www.instagram.com/drjulianaking"
+                icon={SiInstagram}
+                text="drjulianaking"
+                label="instagram"
+              />
+              <SocialLink
+                href="mailto:julianadejongh@gmail.com"
+                icon={SiMailDotRu}
+                text="email"
+                label="email"
+              />
+            </Stack>
+          </Flex>
           <Text pt={8}>Website currently under construction</Text>
         </VStack>
       </Grid>
