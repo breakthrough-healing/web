9b48a2dcfb48eb3d178ad527b181dc3414298314
clifton <cliftonk@gmail.com>
2021-03-18 23:56:16 +0000

    Deploying to gh-pages from @ breakthrough-healing/web@a5507d41d9e3bb97a0d42e95800273acf71a4c7b 🚀

--- _next/static/lDDprYUdBSuCHvWktOw33/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1bfc9850.df5aee8aa81b597a5898.js","static\u002Fchunks\u002Fpages\u002Findex-4533151bdff1918954a4.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9eb534a7d6aa1e5f363f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
