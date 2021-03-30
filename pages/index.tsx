2a35f08e90e789b8042d6dedc3299b668599daee
Clifton King <cliftonk@gmail.com>
2021-03-30 17:30:44 -0500

    improve look on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const IndexPage: FunctionComponent = () => (
   <Layout title="Breakthrough Healing">
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3} mt="5vh">
-        <VStack spacing={[2, 4]}>
+        <VStack spacing={[3, 4]}>
           <Text
             fontFamily="natural"
             fontSize={['3.2rem', '4rem', '5rem']}
@@ -45,29 +45,29 @@ const IndexPage: FunctionComponent = () => (
           <Text
             fontFamily="natural"
             fontStyle="italic"
-            fontSize={['1.5rem']}
+            fontSize={['1.6rem']}
             lineHeight={1}
             pt={2}
           >
-            By Dr. Juliana King, DACM
+            Dr. Juliana King, DACM
           </Text>
-          <Flex pt={2}>
-            <Text pr={3}>Contact me:</Text>
-            <Stack textAlign="left">
-              <SocialLink
-                href="mailto:julianadejongh@gmail.com"
-                icon={SiMailDotRu}
-                text="hello@drjulianaking.com"
-                label="email"
-              />
-              <SocialLink
-                href="https://www.instagram.com/drjulianaking"
-                icon={SiInstagram}
-                text="drjulianaking"
-                label="instagram"
-              />
-            </Stack>
-          </Flex>
+          <Text pr={3} pt={2}>
+            Contact me:
+          </Text>
+          <Stack textAlign="left">
+            <SocialLink
+              href="mailto:julianadejongh@gmail.com"
+              icon={SiMailDotRu}
+              text="hello@drjulianaking.com"
+              label="email"
+            />
+            <SocialLink
+              href="https://www.instagram.com/drjulianaking"
+              icon={SiInstagram}
+              text="drjulianaking"
+              label="instagram"
+            />
+          </Stack>
           <Text pt={8}>Website currently under construction</Text>
         </VStack>
       </Grid>
